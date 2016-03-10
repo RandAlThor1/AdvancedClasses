806c896f55ee20ec8173bd0eedd4797bd653f30e
Julian Kresz <j.kresz@sjasd.ca>
2016-03-10 10:49:15 -0600

    Stuff

---
+++ AdvancedClasses/src/advancedclasses/AdvancedClasses.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package advancedclasses;
+
+/**
+* Author: Julian Kresz
+* Assignment: ASSIGNMENT
+* Teacher: Mr. Wachs
+* Course: Computer Science 40S
+* Created on: Month Day, Year, Time AM
+*
+* ASSIGNMENT.Java:
+*/
+public class AdvancedClasses {
+    
+    public static void main(String[] args) {
+        // Ayyyyyyyyyyyy TODO code application logic here
+    }
+
+}
