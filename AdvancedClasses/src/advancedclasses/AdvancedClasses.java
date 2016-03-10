657ccbfc813a195c7aee63721aaaf440bc34319f
piing3 <d.holmberg@sjasd.ca>
2016-03-10 10:32:30 -0600

    sout poop

--- AdvancedClasses/src/advancedclasses/AdvancedClasses.java
+++ AdvancedClasses/src/advancedclasses/AdvancedClasses.java
@@ -15,7 +15,7 @@ public class AdvancedClasses {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("poop");
     }
     
 }
